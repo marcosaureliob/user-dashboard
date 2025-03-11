22e4a34ce37cee53ba891ffce93017fba2769712
Marcos <marcos.braz@grt8.com.br>
2025-03-11 20:41:56 -0300

    feat:some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Routes from "./router";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
